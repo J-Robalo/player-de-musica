6a92db1fe6078f33f4dd6c2112ddbeb619458319
João Robalo <joao27of@gmail.com>
2023-01-06 21:45:20 -0300

    Acrescentando função de fazer a barrinha de tempo/duração da música andar

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ document.querySelector(".botao-play").addEventListener("click", tocarMusica);
 
 document.querySelector(".botao-pause").addEventListener("click", pausarMusica);
 
+musica.addEventListener("timeupdate", atualizarBarra);
+
 function tocarMusica() {
   musica.play();
   document.querySelector(".botao-pause").style.display = "block";
@@ -14,4 +16,10 @@ function pausarMusica() {
   musica.pause();
   document.querySelector(".botao-pause").style.display = "none";
   document.querySelector(".botao-play").style.display = "block";
+}
+
+function atualizarBarra() {
+  let barra = document.querySelector("progress");
+  barra.style.width =
+    Math.floor((musica.currentTime / musica.duration) * 100) + "%";
 }
